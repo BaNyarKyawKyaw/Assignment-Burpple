630b62d4ad376dd409f2c63d894d940c906f17e5
Ba Nyar Kyaw Kyaw <tmmbnkk1999@gmail.com>
2018-10-11 01:08:29 +0630

    Added Some Component for MVP

---
+++ app/src/main/java/com/example/hp/assignmentburpple/mvp/presenters/MainPresenter.java
@@ -0,0 +1,15 @@
+package com.example.hp.assignmentburpple.mvp.presenters;
+
+import com.example.hp.assignmentburpple.mvp.views.MainView;
+
+public class MainPresenter extends BasePresenter implements MainView {
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
---
+++ app/src/main/java/com/example/hp/assignmentburpple/mvp/views/MainView.java
@@ -0,0 +1,4 @@
+package com.example.hp.assignmentburpple.mvp.views;
+
+public interface MainView {
+}
